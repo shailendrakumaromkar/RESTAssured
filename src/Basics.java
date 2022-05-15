ef71b7c5606b937d63844bb562d557c5e3590a07
eur\om <om@softwareag.com>
2022-05-15 15:10:35 +0530

    First rest assured commit

---
+++ src/Basics.java
@@ -0,0 +1,39 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import files.payload;
+
+
+public class Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RestAssured.baseURI= "https://rahulshettyacademy.com/";
+		String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
+		.body(payload.AddPlace())
+		.when().post("maps/api/place/add/json")
+		.then().assertThat().statusCode(200).body("scope", equalTo("APP"))
+		.header("Server", "Apache/2.4.41 (Ubuntu)").extract().response().asString();
+		
+		JsonPath js = new JsonPath(response);
+		String placeId= js.get("place_id");
+		
+		System.out.println(placeId);
+		
+		
+		//update
+		given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+		.body("{\r\n"
+				+ "\"place_id\":\""+placeId+"\",\r\n"
+				+ "\"address\":\"70 winter walk, USA\",\r\n"
+				+ "\"key\":\"qaclick123\"\r\n"
+				+ "}")
+		.when().put("maps/api/place/update/json")
+		.then().assertThat().log().all().statusCode(200).body("msg",equalTo("Address successfully updated"));
+		
+	}
+
+}

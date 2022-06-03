76cce97332005087e3ee5fb43b354cd6a8a6ccae
shailendrakumaromkar <shailendrakumaromkar@gmail.com>
2022-06-04 00:23:26 +0530

    updated test case

--- src/files/payload.java
+++ src/files/payload.java
@@ -4,7 +4,7 @@ public class payload {
 
 	
 	public static String AddPlace() {
-		//Below is the sample payloads
+		//Below is the sample payload
 		return "{\r\n"
 				+ "  \"location\": {\r\n"
 				+ "    \"lat\": -38.383494,\r\n"
